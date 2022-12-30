f4d7487987015cf2f2de4d730ea6816e68019ab8
vladsorokin <vladsorokin636@gmail.com>
2022-12-30 23:30:04 +0300

    test commit

---
+++ src/ru/mirea/task16/App.java
@@ -0,0 +1,12 @@
+package ru.mirea.task16;
+
+public class App {
+    public static void main(String[] args) {
+        // Игра
+        new task1_1();
+        // Мышка
+        new task1_2();
+        // Меню
+        new task2_1();
+    }
+}
